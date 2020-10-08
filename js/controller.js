47414af64ae3753caa5e07959a663784bfcf5477
yang-520199 <951333953@qq.com>
2020-10-08 21:43:56 +0800

    first commit

---
+++ js/controller.js
@@ -0,0 +1,35 @@
+function f() {
+    var bar = document.getElementById("bar");
+    if (bar.style.display == "none") {
+        bar.style.display = "";
+    } else {
+        bar.style.display = "none"
+    }
+
+}
+
+function s() {
+    var bar = document.getElementById("showdata");
+    if (bar.style.display == "none") {
+        bar.style.display = "";
+    } else {
+        bar.style.display = "none";
+    }
+
+}
+
+function pic() {
+    var bar = document.getElementById("bar");
+    if (bar.style.display == "none") {
+        bar.style.display = "";
+    } else {
+        bar.style.display = "none";
+    }
+    var bar = document.getElementById("show");
+    if (bar.style.display == "none") {
+        bar.style.display = "";
+    } else {
+        bar.style.display = "none";
+    }
+
+}
